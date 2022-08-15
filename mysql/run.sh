e96cc9b31e9364c7c4609b8c8c14ea35203a1597
Aleksy Ruszala <aleksy.ruszala@n-able.com>
2022-08-15 12:23:23 +0100

    1

--- mysql/run.sh
+++ mysql/run.sh
@@ -4,9 +4,12 @@ TRIES=3
 
 cat queries.sql | while read query; do
     sync
-    echo 3 | sudo tee /proc/sys/vm/drop_caches
+    echo 3 | tee /proc/sys/vm/drop_caches
+    echo "$(date +%s)  $1 droped"
 
     for i in $(seq 1 $TRIES); do
-        sudo mysql test -vvv -e "${query}"
+        echo "$(date +%s)  $1 test ${i}"
+        #sudo mysql -h localhost -P 3333 -u root -psecret mysqlslap -vvv -e "${query}"
+        winpty docker run -it mysql --rm mysql -h localhost -P 3333 -u root -psecret mysqlslap -vvv -e "SELECT * FROM t1 WHERE charcol1 LIKE '%ag%' ORDER BY intcol2 LIMIT 1;" 2>&1
     done;
 done;
---
+++ mysql/test.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+
+for i in `seq 5`
+do
+        {
+          echo " $(date +%s) start ${i}"
+               ./run.sh ${i}
+        }&              #The middle section is directly executed in the background
+
+done
+wait                    #After all the tasks put in the background are completed, the
+echo "END"
